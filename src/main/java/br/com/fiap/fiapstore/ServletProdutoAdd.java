e8fc016214b641fc10fc99c1ab584eb76be7b167
Falandimho <anthonygame311@gmail.com>
2025-05-05 22:40:20 -0300

    Servlets

---
+++ src/main/java/br/com/fiap/fiapstore/ServletProdutoAdd.java
@@ -0,0 +1,30 @@
+package br.com.fiap.fiapstore;
+
+import java.io.*;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "produto", value = "/produto-servlet")
+public class ServletProduto extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String nome = request.getParameter("nome");
+        int qtd = Integer.parseInt(request.getParameter("quantidade"));
+        double preco = Double.parseDouble(request.getParameter("preco"));
+
+        System.out.println("Nome: " + nome +
+                ", Quantidade: " + qtd +
+                ", Preço: " + preco);
+
+        request.setAttribute("nome", nome);
+        request.setAttribute("quantidade", qtd);
+        request.setAttribute("preco", preco);
+
+        request.getRequestDispatcher("sucesso.jsp").forward(request, response);
+    }
+
+    public void destroy() {
+    }
+}
